5021fb880ead1c6931369d0bed9cc900d79ddbc1
rubennicolas <anjenidi@gmail.com>
2025-08-24 15:33:46 +0200

    Nueva actualización de logs en Health Controller

--- src/main/java/com/rubennicolas/email_server_spring_boot/controllers/HealthController.java
+++ src/main/java/com/rubennicolas/email_server_spring_boot/controllers/HealthController.java
@@ -1,13 +1,17 @@
 package com.rubennicolas.email_server_spring_boot.controllers;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping
+@Slf4j
 public class HealthController {
 
     @GetMapping("/health")
     public String checkServiceHealth() {
+        log.info("Checking service health every 5 minutes from GitHub Actions");
+
         return "OK";
     }
 }

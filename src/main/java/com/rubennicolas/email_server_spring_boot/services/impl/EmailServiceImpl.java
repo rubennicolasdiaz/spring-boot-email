da7f19a47d69c5e42bcde98ceca7da4502fdd506
rubennicolas <anjenidi@gmail.com>
2025-10-02 19:06:03 +0200

    Corrección del contenido de contentHtml

--- src/main/java/com/rubennicolas/email_server_spring_boot/services/impl/EmailServiceImpl.java
+++ src/main/java/com/rubennicolas/email_server_spring_boot/services/impl/EmailServiceImpl.java
@@ -43,7 +43,7 @@ public class EmailServiceImpl implements IEmailService {
 
             String contentHtml = templateEngine.process("principal", context);
 
-            helper.setText(emailDTO.getMessage(), contentHtml);
+            helper.setText(contentHtml, true);
 
             javaMailSender.send(mimeMessage);
 

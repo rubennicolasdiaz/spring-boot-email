4b41676fb604aa78274655f3a650f679c0ca48de
rubennicolas <anjenidi@gmail.com>
2025-10-03 20:53:24 +0200

    Actualización de CORS con el nuevo dominio de la web

--- src/main/java/com/rubennicolas/email_server_spring_boot/config/CorsConfig.java
+++ src/main/java/com/rubennicolas/email_server_spring_boot/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("https://rubennicolasdiaz.github.io")
+                        .allowedOrigins("https://www.rubennicolasdiaz.me")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*");
             }
